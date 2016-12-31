b20553da7757e53e842435be98c96c82a24c6868
Tarun <trikha.tarun01@gmail.com>
2016-12-31 19:39:11 +0530

    Updated radio button menu

--- app/src/main/java/com/android/movies/MainActivity.java
+++ app/src/main/java/com/android/movies/MainActivity.java
@@ -1,17 +1,20 @@
 package com.android.movies;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private Boolean sort_popularity,sort_votes;
+    private MenuItem popularityButton, votesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +23,26 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         sort_popularity = preferences.getBoolean(getString(R.string.popularity),true);
         sort_votes = preferences.getBoolean(getString(R.string.votes),false);
+
+        Log.v("Popularity: ", sort_popularity.toString());
+        Log.v("Votes: ", sort_votes.toString());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu,menu);
+        popularityButton = menu.findItem(R.id.popularity_button);
+        votesButton = menu.findItem(R.id.votes_button);
+        if (sort_popularity)
+            popularityButton.setChecked(true);
+        else
+            votesButton.setChecked(true);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        MenuItem popularityButton,votesButton;
-        popularityButton = (MenuItem) findViewById(R.id.popularity_button);
-        votesButton = (MenuItem) findViewById(R.id.votes_button);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = preferences.edit();
 
@@ -45,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
                 editor.putBoolean(getString(R.string.popularity),true);
                 editor.putBoolean(getString(R.string.votes),false);
                 editor.commit();
+                restartActivity();
             }
         }
         else {
@@ -55,8 +65,19 @@ public class MainActivity extends AppCompatActivity {
                 editor.putBoolean(getString(R.string.popularity),false);
                 editor.putBoolean(getString(R.string.votes),true);
                 editor.commit();
+                restartActivity();
             }
         }
         return true;
+    }
+
+    void restartActivity() {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
     }
 }
