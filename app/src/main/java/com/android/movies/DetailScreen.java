ed94925c47d30f728bcc9897a01f5787e355ab1c
Tarun <trikha.tarun01@gmail.com>
2017-01-03 12:51:37 +0530

    Completed detail activity. Completed project

--- app/src/main/java/com/android/movies/DetailScreen.java
+++ app/src/main/java/com/android/movies/DetailScreen.java
@@ -2,8 +2,10 @@ package com.android.movies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailScreen extends AppCompatActivity {
 
@@ -11,11 +13,32 @@ public class DetailScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         Bundle dataPack = getIntent().getExtras();
         Movie currentMovie = (Movie) dataPack.get("currentMovie");
-        Log.v("Class passed: ", currentMovie.getmName());
+
+        String title, synopsis, posterUrl, rating, releaseDate;
+
+        title = currentMovie.getmName();
+        synopsis = currentMovie.getmSynopsis();
+        posterUrl = currentMovie.getmImageUrl();
+        rating = currentMovie.getmRating();
+        releaseDate = currentMovie.getmReleaseDate();
+
+        setTitle(title);
+
+        TextView synopsis_tv, rating_tv, releaseDate_tv;
+
+        ImageView poster = (ImageView) findViewById(R.id.poster);
+
+        Picasso.with(this).load(posterUrl).into(poster);
+
+        synopsis_tv = (TextView) findViewById(R.id.synopsis_content);
+        rating_tv = (TextView) findViewById(R.id.rating);
+        releaseDate_tv = (TextView) findViewById(R.id.release_date);
+
+        synopsis_tv.setText(synopsis);
+        releaseDate_tv.setText(releaseDate);
+        rating_tv.setText(rating);
     }
 
 }
